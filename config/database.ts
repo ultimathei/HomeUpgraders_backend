cf9543562aebca29b61d95e11f1d82113ff84e57
ultimathei <krisztian.mate.design@gmail.com>
2022-11-24 19:53:54 +0000

    update db details

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'hup_db'),
-      user: env('DATABASE_USERNAME', 'hup_admin'),
-      password: env('DATABASE_PASSWORD', 'c0mm@nd+V'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      // ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
